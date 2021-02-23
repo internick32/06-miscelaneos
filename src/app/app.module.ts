8e5d52bee27c9cebddee120e48ada37a12ce301f
Trigalo27 <internick27@gmail.com>
2021-02-23 12:19:50 -0600

    commit a 06-miscelaneis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './componets/ng-style/ng-style.component';
+import { CssComponent } from './componets/css/css.component';
+import { ClasesComponent } from './componets/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './componets/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componets/css/css.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  template: `
+    <p>
+      css works!
+    </p>
+  `,
+  styles: [`
+    p { font-size:20px; color: red; }
+  `]
+})
+export class CssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
